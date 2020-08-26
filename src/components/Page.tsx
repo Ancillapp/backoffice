20cc7a518706a424c1a3d446a0e96d8fdab77645
Giorgio Garasto <giorgio@garasto.it>
2020-08-26 13:22:37 +0200

    feat: improve page component layout

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -9,27 +9,42 @@ export interface PageProps extends BoxProps {
 
 const useStyles = makeStyles<Theme, PageProps>((theme) => ({
   root: ({ size = 'sm' }) => ({
-    padding: '2rem 3rem',
-    background: theme.palette.background.paper,
-    width: '100%',
-    maxWidth: typeof size === 'number' ? size : theme.breakpoints.width(size),
     margin: 0,
+    width: '100%',
     flex: '1 1 auto',
+    display: 'flex',
+    alignItems: 'stretch',
 
     [theme.breakpoints.up('md')]: {
       flex: '0 0 auto',
-      margin: `${theme.spacing(3)}px auto`,
-      borderRadius: '.5rem',
-      boxShadow:
-        '0 .06rem .065rem 0 rgba(0, 0, 0, 0.14), 0 .003rem .15rem 0 rgba(0, 0, 0, 0.12), 0 .09rem .0035rem -.065rem rgba(0, 0, 0, 0.2)',
+      margin: theme.spacing(3),
+      width: `calc(100% - ${theme.spacing(6)}px)`,
+    },
+
+    '& > *': {
+      padding: '2rem 3rem',
+      background: theme.palette.background.paper,
+      width: '100%',
+      maxWidth: typeof size === 'number' ? size : theme.breakpoints.width(size),
+      margin: '0 auto',
+
+      [theme.breakpoints.up('md')]: {
+        borderRadius: '.5rem',
+        boxShadow:
+          '0 .06rem .065rem 0 rgba(0, 0, 0, 0.14), 0 .003rem .15rem 0 rgba(0, 0, 0, 0.12), 0 .09rem .0035rem -.065rem rgba(0, 0, 0, 0.2)',
+      },
     },
   }),
 }));
 
-const Page: FunctionComponent<PageProps> = (props) => {
+const Page: FunctionComponent<PageProps> = ({ children, ...props }) => {
   const classes = useStyles(props);
 
-  return <Box component="section" className={classes.root} {...props} />;
+  return (
+    <Box className={classes.root} {...props}>
+      <Box component="section">{children}</Box>
+    </Box>
+  );
 };
 
 export default Page;
