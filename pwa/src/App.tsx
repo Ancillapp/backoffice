5a6924bf9908d9b00a1b60d817bb83be69afc945
Giorgio Garasto <giorgio@garasto.it>
2020-08-27 09:19:16 +0200

    feat: setup firebase

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -4,20 +4,29 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 
+import type { app } from 'firebase';
+
 import lightTheme from './themes/light';
 
 import Root from './containers/Root';
+import FirebaseProvider from './providers/FirebaseProvider';
 import ServiceWorkerProvider from './providers/ServiceWorkerProvider';
 
-const App: FunctionComponent = () => (
-  <ThemeProvider theme={lightTheme}>
-    <CssBaseline />
-    <ServiceWorkerProvider>
-      <BrowserRouter>
-        <Root />
-      </BrowserRouter>
-    </ServiceWorkerProvider>
-  </ThemeProvider>
+export interface AppProps {
+  firebase: app.App;
+}
+
+const App: FunctionComponent<AppProps> = ({ firebase }) => (
+  <FirebaseProvider firebase={firebase}>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <ServiceWorkerProvider>
+        <BrowserRouter>
+          <Root />
+        </BrowserRouter>
+      </ServiceWorkerProvider>
+    </ThemeProvider>
+  </FirebaseProvider>
 );
 
 export default App;
--- pwa/src/index.tsx
+++ pwa/src/index.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
+import { initializeApp } from 'firebase/app';
+
+import 'firebase/auth';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+ReactDOM.render(<App firebase={firebase} />, document.getElementById('root'));
