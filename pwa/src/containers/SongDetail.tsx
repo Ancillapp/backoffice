72786e127b57681ecb90e4f5ebd769d13c0775eb
Giorgio Garasto <giorgio@garasto.it>
2020-08-27 16:51:16 +0200

    feat: setup song form submit and reset

--- pwa/src/containers/SongDetail.tsx
+++ pwa/src/containers/SongDetail.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useCallback, useState } from 'react';
 
-import { Link, useRouteMatch } from 'react-router-dom';
+import { Link, LinkProps, useRouteMatch } from 'react-router-dom';
 
 import { Box, IconButton } from '@material-ui/core';
 
@@ -14,7 +14,7 @@ import { useSong } from '../providers/ApiProvider';
 
 import TopbarLayout, { TopbarLayoutProps } from '../components/TopbarLayout';
 import Loader from '../components/Loader';
-import SongForm, { SongLanguage } from '../components/SongForm';
+import SongForm, { SongFormProps, SongLanguage } from '../components/SongForm';
 
 const mapSongNumberToLanguage = (number: string): SongLanguage =>
   number.startsWith('DE') ? SongLanguage.GERMAN : SongLanguage.ITALIAN;
@@ -24,6 +24,7 @@ const SongDetail: FunctionComponent<Omit<
   'startAdornment'
 >> = (props) => {
   const [editMode, setEditMode] = useState(false);
+  const [updatingSong, setUpdatingSong] = useState(false);
 
   const {
     params: { number },
@@ -33,6 +34,32 @@ const SongDetail: FunctionComponent<Omit<
 
   const toggleEditMode = useCallback(() => {
     setEditMode((oldEditMode) => !oldEditMode);
+  }, []);
+
+  const handleBackClick = useCallback<NonNullable<LinkProps['onClick']>>(
+    (event) => {
+      if (updatingSong) {
+        event.preventDefault();
+      }
+    },
+    [updatingSong],
+  );
+
+  const handleSubmit = useCallback<NonNullable<SongFormProps['onSubmit']>>(
+    (data) => {
+      setUpdatingSong(true);
+
+      // TODO: actually update the song
+      console.log(data);
+
+      setUpdatingSong(false);
+      setEditMode(false);
+    },
+    [],
+  );
+
+  const handleReset = useCallback<NonNullable<SongFormProps['onReset']>>(() => {
+    setEditMode(false);
   }, []);
 
   if (error) {
@@ -48,8 +75,13 @@ const SongDetail: FunctionComponent<Omit<
       }
       startAdornment={
         <Box color="primary.contrastText" marginRight={0.5} clone>
-          <Link to="/canti">
-            <IconButton color="inherit" edge="start" aria-label="indietro">
+          <Link to="/canti" onClick={handleBackClick}>
+            <IconButton
+              color="inherit"
+              edge="start"
+              aria-label="indietro"
+              disabled={updatingSong}
+            >
               <ArrowBackIcon />
             </IconButton>
           </Link>
@@ -63,6 +95,8 @@ const SongDetail: FunctionComponent<Omit<
               aria-label="annulla"
               type="reset"
               form="edit-song-form"
+              key="cancel-editing-song-button"
+              disabled={updatingSong}
             >
               <CloseIcon />
             </IconButton>
@@ -72,8 +106,15 @@ const SongDetail: FunctionComponent<Omit<
               aria-label="conferma"
               type="submit"
               form="edit-song-form"
+              key="save-song-button"
+              disabled={updatingSong}
             >
               <DoneIcon />
+              {updatingSong && (
+                <Box color="primary.contrastText" clone>
+                  <Loader size={24} color="inherit" />
+                </Box>
+              )}
             </IconButton>
           </>
         ) : (
@@ -82,6 +123,7 @@ const SongDetail: FunctionComponent<Omit<
               color="inherit"
               aria-label="elimina"
               onClick={console.log}
+              key="delete-song-button"
             >
               <DeleteIcon />
             </IconButton>
@@ -90,6 +132,7 @@ const SongDetail: FunctionComponent<Omit<
               edge="end"
               aria-label="modifica"
               onClick={toggleEditMode}
+              key="edit-song-button"
             >
               <EditIcon />
             </IconButton>
@@ -103,13 +146,14 @@ const SongDetail: FunctionComponent<Omit<
       ) : (
         <SongForm
           id="edit-song-form"
-          disabled={!editMode}
+          disabled={!editMode || updatingSong}
           defaultValue={{
             ...data,
             number: data.number.slice(2),
             language: mapSongNumberToLanguage(data.number),
           }}
-          onSubmit={console.log}
+          onSubmit={handleSubmit}
+          onReset={handleReset}
         />
       )}
     </TopbarLayout>
