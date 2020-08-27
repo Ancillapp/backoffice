b9260b8c1c3a940ddb7e67fe0f84f984d950cede
Giorgio Garasto <giorgio@garasto.it>
2020-08-27 09:13:25 +0200

    feat: setup api authorization

---
+++ functions/src/api/middlewares/authorize.ts
@@ -0,0 +1,32 @@
+import { firebase } from '../../helpers/firebase';
+
+import type { RequestHandler } from 'express';
+
+export const authorize: RequestHandler = async (
+  { headers: { authorization } },
+  res,
+  next,
+) => {
+  res.set('Cache-Control', 'private, no-store');
+
+  const token = authorization?.match(/Bearer (.+)/)?.[1];
+
+  if (!token) {
+    res.status(401).send();
+    return;
+  }
+
+  const decodedToken = await firebase
+    .auth()
+    .verifyIdToken(token)
+    .catch(() => null);
+
+  if (decodedToken?.customClaims?.role !== 'SUPERUSER') {
+    res.status(403).send();
+    return;
+  }
+
+  res.locals.user = decodedToken;
+
+  next();
+};
