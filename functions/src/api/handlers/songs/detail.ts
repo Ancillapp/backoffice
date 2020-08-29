50e75bcfef5769402e7cfcdc7b6bde0b983313df
Giorgio Garasto <giorgio@garasto.it>
2020-08-29 13:48:57 +0200

    fix: disable api caching

--- functions/src/api/handlers/songs/detail.ts
+++ functions/src/api/handlers/songs/detail.ts
@@ -3,11 +3,6 @@ import { get } from '../../../services/song';
 import type { RequestHandler } from 'express';
 
 export const getSong: RequestHandler = async ({ params: { number } }, res) => {
-  res.set(
-    'Cache-Control',
-    'public, max-age=1800, s-maxage=3600, stale-while-revalidate=3600',
-  );
-
   const song = await get(number);
 
   if (!song) {
--- functions/src/api/handlers/songs/list.ts
+++ functions/src/api/handlers/songs/list.ts
@@ -6,11 +6,6 @@ export const getSongs: RequestHandler = async (
   { query: { fullData } },
   res,
 ) => {
-  res.set(
-    'Cache-Control',
-    'public, max-age=1800, s-maxage=3600, stale-while-revalidate=3600',
-  );
-
   const songs = await list(typeof fullData !== 'undefined');
 
   res.json(songs);
